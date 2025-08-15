9a4bd61096ee61f143fd294ae08f3411d10f9ee7
VladislavKovalenko <soros765@gmail.com>
2025-08-15 20:14:49 +0300

    A new branch has been created to work on the entry point

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -1,12 +1,26 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 class  Enemy : MonoBehaviour
 {
-    private EnemyData EnemyData;
+    [SerializeField] private EnemyData EnemyData;
+    private float _enemyHealth;
+    private float _enemySpeed;
+    private float _enemySize;
 
     private void Start()
     {
-        throw new NotImplementedException();
+        InitReset();
     }
+    
+    
+    
+    void InitReset() 
+    {
+        _enemyHealth = EnemyData.Health;
+     //_enemySpeed;
+     //_enemySize;
+    }
+    
 }
