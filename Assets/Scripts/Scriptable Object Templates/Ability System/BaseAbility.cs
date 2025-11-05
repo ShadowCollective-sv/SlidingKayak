83d1da8c04a5fd2e6b8e1115a10bb6e7f7aeb1e9
VladislavKovalenko <soros765@gmail.com>
2025-11-06 00:02:01 +0300

    Начало работы над Ability System

---
+++ Assets/Scripts/Scriptable Object Templates/Ability System/BaseAbility.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace HauntedHouses
+{
+    [CreateAssetMenu (fileName = "Ability", menuName = "ScriptableObjects/AbilitySystem/Ability")]
+    public class BaseAbility : ScriptableObject
+    {
+        
+    }
+}
